ffc79bede0799580cdf678f6f4e8cb44b17685a7
denis0001-dev <denis0001.dev@ya.ru>
2025-08-15 12:10:49 +0300

    Change base URL to proxy to the backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,13 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 8301,
-        strictPort: true
+        strictPort: true,
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:8300/",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, "")
+            }
+        }
     }
 })
