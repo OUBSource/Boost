a17c850ba162f51c8231ff7b6a8d9681148a777b
denis0001-dev <denis0001.dev@ya.ru>
2025-08-15 13:44:36 +0300

    Improve build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+    plugins: [
+        createHtmlPlugin({
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+                minifyCSS: true,
+                minifyJS: true
+            }
+        })
+    ],
     server: {
         host: '0.0.0.0',
         port: 8301,
@@ -13,5 +29,25 @@ export default defineConfig({
             }
         },
     },
-    appType: "mpa"
+    appType: "mpa",
+    css: {
+        postcss: {
+            plugins: [autoprefixer()],
+        },
+    },
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true
+            }
+        },
+        rollupOptions: {
+            external: [
+                /^webfonts/
+            ]
+        },
+        cssMinify: true
+    }
 })
